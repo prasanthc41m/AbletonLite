3e0a06ca130021b79efc6fec47ad61c3a454c77d
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2023-10-24 10:58:53 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 # To complete
 
-WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Intro ~/wol/Ableton_Live_11_Intro/drive_c/ProgramData/Ableton/"Live 11 Intro"/Program/"Ableton Live 11 Intro.exe"
+WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Lite ~/wol/Ableton_Live_11_Lite/drive_c/ProgramData/Ableton/"Live 11 Lite"/Program/"Ableton Live 11 Lite.exe"
 
