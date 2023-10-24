4aec0fcbd354770a10232a92fe5a2b9c4d4791b6
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2023-10-24 11:46:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ echo "Installing all dependencies for Ableton Live 11 Lite and preparing all env
 WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Lite winetricks d3dx9 dotnet35 dotnet452 dxvk gdiplusvcrun2019 corefonts tahoma vcrun6sp6
 WINEARCH=win64 WINEPREFIX=~/wol/Ableton_Live_11_Lite winetricks win10
 echo "Creating shortcut on your desktop, please don't open it until the installer finish its work"
-cp ~/wol/Downloads/"AbletonLite"/startup.sh ~/wol/Ableton_Live_11_Lite
-cp ~/wol/Downloads/"AbletonLite"/icon.png ~/wol/Ableton_Live_11_Lite
-cp ~/wol/Downloads/"AbletonLite"/ableton.desktop $desktop
+cp startup.sh ~/wol/Ableton_Live_11_Lite
+cp icon.png ~/wol/Ableton_Live_11_Lite
+cp ableton.desktop $desktop
 cd ~/wol/Ableton_Live_11_Lite/drive_c
 #echo "Downloading Ableton Live 11 Lite, this depends on your internet connection speed, Please wait..."
 #wget https://cdn-downloads.ableton.com/channels/11.1.5/ableton_live_lite_11.1.5_64.zip -O ~/wol/Ableton_Live_11_Lite/drive_c/ableton.zip
